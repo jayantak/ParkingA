2e2921946bf84d02d86c3118b4e82a3192314ced
jayantak <jayanta.k@outlook.com>
2015-06-04 12:52:04 +0530

    FourthAndFifthTests

--- main.js
+++ main.js
@@ -1,13 +1,41 @@
 var parkingLot = function()
 {
+  var max = 3;
+  var fsb = 0;
+  var carsParked = 0;
   var spaces = new Array();
   }
 
+parkingLot.initialise = function()
+{
+  max = 3;
+  fsb = 0;
+  carsParked = 0;
+  spaces = new Array();
+}
 
+parkingLot.checkLimit = function()
+{
+  return max;
+}
+
+parkingLot.checkFull = function()
+{
+  if(carsParked >= max)
+  {
+    fsb = 1;
+  }
+  else
+  {
+    fsb = 0;
+  }
+  return fsb;
+
+}
 
   parkingLot.parking = function(id, spaces)
   {
-    for(i = 0; i<=spaces.length; i++)
+    for(i = 0; (i<=spaces.length)&&(parkingLot.checkFull()==0); i++)
     {
       if(spaces[i]==id)
       {
@@ -16,7 +44,9 @@ var parkingLot = function()
       }
       if(spaces[i]==undefined)
       {
+        carsParked++;
         spaces[i] = id;
+        parkingLot.checkFull();
         // document.getElementById('output1').innerHTML = "The car has been parked at spot number "+(i+1);
         break;
       }
@@ -27,14 +57,17 @@ var parkingLot = function()
 
   parkingLot.unParking = function(id, spaces)
   {
+
     var found = 0;
     for(i=0; i<spaces.length; i++)
     {
       if(spaces[i]==id)
       {
         spaces[i] = undefined;
+        carsParked--;
         // document.getElementById('output2').innerHTML = "Your car was found at spot number "+(i+1);
         found = 1;
+        parkingLot.checkFull();
         break;
       }
     }
--- test/test.js
+++ test/test.js
@@ -7,31 +7,36 @@ describe('Parking', function()
   it('Spot should contain current car once parked', function()
   {
     var spaceTest = new Array();
+    pLot.initialise();
     spaceTest = pLot.parking("AAA", spaceTest);
     assert.equal(spaceTest[0], "AAA", spaceTest);
 
   })
+
   it('Spot should be empty when unparked and other spaces must remain the same', function()
   {
     var spaceTest = new Array();
+    pLot.initialise();
     spaceTest = pLot.parking("AAA", spaceTest);
     spaceTest = pLot.parking("BBB", spaceTest);
     spaceTest = pLot.unParking("AAA", spaceTest);
     assert.equal(spaceTest[0], undefined);
     assert.equal(spaceTest[1], "BBB")
   })
+
   it('When a spot is used, all spots before it must be occupied', function()
   {
     var spaceTest = new Array();
-    var testLength = 20, testID, i;
+    pLot.initialise();
+    var testLength = pLot.checkLimit(), testID, i;
     for(i = 0; i<testLength; i++)
     {
       testID = 2*i;
       spaceTest = pLot.parking(testID.toString(), spaceTest);
     }
-    testID-=6;
+    testID-=2;
     spaceTest = pLot.unParking(testID.toString(), spaceTest);
-    testID-=6;
+    testID-=2;
     spaceTest = pLot.unParking(testID.toString(), spaceTest);
 
     spaceTest = pLot.parking("TEST", spaceTest);
@@ -42,5 +47,34 @@ describe('Parking', function()
     {
       assert.notEqual(spaceTest[i], undefined);
     }
+  })
+
+  it('Check should return full when parking lot is full', function()
+  {
+    var spaceTest = new Array();
+    pLot.initialise();
+    var testLength = pLot.checkLimit();
+    for(i = 0; i<testLength; i++)
+    {
+      testID = 2*i;
+      spaceTest = pLot.parking(testID.toString(), spaceTest);
+
+    }
+    assert.equal(pLot.checkFull(), 1);
+  })
+
+  it('Check should return not full when there is space', function()
+  {
+    var spaceTest = new Array();
+    pLot.initialise();
+    var testLength = pLot.checkLimit();
+    for(i = 0; i<testLength; i++)
+    {
+      testID = 2*i;
+      spaceTest = pLot.parking(testID.toString(), spaceTest);
+
+    }
+    spaceTest = pLot.unParking("2", spaceTest);
+    assert.equal(pLot.checkFull(), 0);
   })
 });
