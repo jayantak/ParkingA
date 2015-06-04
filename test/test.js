cdef673caccfc9c5a692abc93e42d8d6c929d9f9
jayantak <jayanta.k@outlook.com>
2015-06-04 09:32:48 +0530

    Changed Second Test
    Second test slightly expanded

--- test/test.js
+++ test/test.js
@@ -11,13 +11,14 @@ describe('Parking', function()
     assert.equal(spaceTest[0], "AAA", spaceTest);
 
   })
-  it('Spot should be empty when unparked', function()
+  it('Spot should be empty when unparked and other spaces must remain the same', function()
   {
     var spaceTest = new Array();
     spaceTest = pLot.parking("AAA", spaceTest);
     spaceTest = pLot.parking("BBB", spaceTest);
     spaceTest = pLot.unParking("AAA", spaceTest);
     assert.equal(spaceTest[0], undefined);
+    assert.equal(spaceTest[1], "BBB")
   })
   it('When a spot is used, all spots before it must be occupied', function()
   {
